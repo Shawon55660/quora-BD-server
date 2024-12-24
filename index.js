8f63f389c693969674f3c76bfb93d55195b35a5f
shawonahme <shawonahmed55660@gmail.com>
2024-12-24 15:28:34 +0600

    jwt api and token done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 3000
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //mideware
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionalSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}@cluster0.u5q3a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,8 +34,52 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+//verifyToken
+
+const verifyToken = (req,res,next)=>{
+  const token = req.cookies?.token;
+  if(!token) return res.status(401).send({messsage:'unauthorized access'})
+    jwt.verify(token,process.env.TOKEN_KEY,(error,decoded)=>{
+  if(error){
+    return res.status(401).send({messsage:'unauthorized access'})
+  }
+  req.user = decoded
+})
+next()
+}
 async function run() {
   try {
+
+
+    // create jwt api start here 
+
+    //token crate in cookie here
+    app.post('/jwt', async(req,res)=>{
+      const email = req.body
+      const token = jwt.sign(email, process.env.TOKEN_KEY,{expiresIn:'1h'});
+      res.cookie('token',token,{
+        httpOnly:true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({sucess:true})
+    })
+
+    //clear cookie get api
+
+    app.get('/tokenLogout', async(req,res)=>{
+      res.clearCookie('token',{
+        maxAge: 0,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({succesc:true})
+    })
+
+
+
     const db = client.db('quora');
     const quoraCollections = db.collection('quoraCollections')
     const recommandCollection = db.collection('recommandCollection')
@@ -42,6 +94,17 @@ async function run() {
 
     })
 
+
+    // query all data get  api
+
+    app.get('/Recent-queries', async (req, res) => {
+      const data = req.body;
+      
+     
+
+      const result = await quoraCollections.find(data).sort({ currentData: -1 }).limit(6).toArray()
+      res.send(result)
+    })
     // query all data get  api
 
     app.get('/all-query', async (req, res) => {
@@ -60,11 +123,17 @@ async function run() {
 
     // myQuery data get with my email api
 
-    app.get('/my-query', async (req, res) => {
+    app.get('/my-query', verifyToken, async (req, res) => {
       const email = req.query.owner_email;
+      const decodedEmail = req.user?.email
+      
+      if (decodedEmail !== email)
+        return res.status(401).send({ message: 'unauthorized access' })
+     else{
       const filter = { owner_email: email }
       const result = await quoraCollections.find(filter).sort({ currentData: -1 }).toArray();
       res.send(result)
+     }
 
 
     })
@@ -124,8 +193,10 @@ async function run() {
     })
 
     // my recommendation get by recommand_email API
-    app.get('/my-recommendation', async (req, res) => {
+    app.get('/my-recommendation', verifyToken, async (req, res) => {
       const email = req.query.recommender_email;
+      const decoded = req.user?.email;
+      if(decoded!== email) return res.status(401).send({message:'permisson not allowed'})
       const query = { recommender_email: email };
       const result = await recommandCollection.find(query).toArray()
       res.send(result)
@@ -153,11 +224,17 @@ async function run() {
     })
 
     //  recommendation for me get by recommand_email API
-    app.get('/recommendation-for-me', async (req, res) => {
+    app.get('/recommendation-for-me', verifyToken, async (req, res) => {
       const email = req.query.owner_email;
+      const decodedEmail = req.user?.email
+      if (decodedEmail !== email)
+      return res.status(401).send({ message: 'unauthorized access' })
+
+     else{
       const query = { owner_email: email };
       const result = await recommandCollection.find(query).toArray()
       res.send(result)
+     }
     })
 
     // recommendation by id api
@@ -167,6 +244,10 @@ async function run() {
       const result = await recommandCollection.find(filter).toArray();
       res.send(result)
     })
+
+
+
+    
 
   } finally {
 
